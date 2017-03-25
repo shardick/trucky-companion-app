cf69ff6672ee2f7a069db1daacc6b552dfb20484
Shardick <shardick@gmail.com>
2017-03-25 17:22:54 +0100

    joined all js

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
+var concat = require('gulp-concat');
 
 var sassPaths = [
   'bower_components/normalize.scss/sass',
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src',
   'bower_components/font-awesome/scss'
+];
+
+var jsPath = [
+  'bower_components/jquery/dist/jquery.js',
+  'bower_components/foundation-sites/dist/js/foundation.js',
+  'bower_components/wow/dist/wow.js'
 ];
 
 gulp.task('sass', function() {
@@ -21,6 +28,13 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('joinjs', function() {
+
+  return gulp.src(jsPath)
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('default', ['sass', 'joinjs'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
 });
