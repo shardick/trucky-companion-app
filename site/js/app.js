5d5a3369c4dd5f66ec83c6f7babaeec1a56034e4
Shardick <shardick@gmail.com>
2017-03-25 17:18:11 +0100

    app site

--- site/js/app.js
+++ site/js/app.js
@@ -1,1 +1,17 @@
-$(document).foundation();
+(function($) {
+  "use strict";
+
+  $(document).foundation();
+
+  //Run When Document Ready
+  $(document).on('ready', function() {
+    initWow();
+  });
+
+  //WOW Animation
+  //===============================================================================
+  function initWow() {
+    new WOW().init();
+  }
+
+})(jQuery);
